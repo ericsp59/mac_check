13e6b201a17bb4c501377d5c75cb6e1131e27464
ericsp <ericsp@mail.ru>
2021-12-03 09:39:36 +0500

    add json

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,19 +8,35 @@ function App() {
           MAC_CHECK! 
         </h1>
         <input className="mac-check_input" />
-        <button href='http://192.168.50.22:8080' onClick={getData} className='mac-check_btn'>check</button>
+        <button onClick={getData} className='mac-check_btn'>check</button>
+        <OutputBlock />
       </header>
+      
     </div>
   );
 }
 
-async function getData(e){
+function OutputBlock() {
+  let x = '123123123'
+  function setX(a) {
+    this.x = a
+  } 
+  return (
+    <div>
+      <span>{x}</span>
+    </div>
+  );
+}
+
+async function getData(){
   // e.preventDefault();
   console.log(123)
   try {
     const response = await fetch('http://192.168.50.22:8080');
-    // const json = await response.json();
-    console.log(response)
+    const json = await response.json();
+    console.log(typeof(json))
+    OutputBlock.setX(json)
+    
   } catch(err) {
     alert(err); // Failed to fetch
   }
