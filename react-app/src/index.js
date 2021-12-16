222e6ab568a989fe52d6817d357eaab7dd237533
ericsp <ericsp@mail.ru>
2021-12-16 08:38:59 +0500

    remove appest

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import AppTest from './AppTest'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* <AppTest/> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
